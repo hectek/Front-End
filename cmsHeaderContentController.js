1fad7bf289e2de16ffcae044d87df7301b1e6885
Hector <ariaspatron@gmail.com>
2017-06-09 13:52:33 -0700

    Add files via upload
    Basic Header Content Controller

---
+++ cmsHeaderContentController.js
@@ -0,0 +1,62 @@
+﻿// cmsHeaderContentController
+
+(function () {
+    "use strict";
+    angular.module(APPNAME)
+    .controller('cmsHeaderContentController', HeaderContentController);
+
+    HeaderContentController.$inject = ['$scope', '$baseController', '$http', '$sce', '$cmsContentService', "$serverModel"]
+
+    function HeaderContentController($scope, $baseController, $http, $sce, $cmsContentService, $serverModel) {
+        var vm = this;
+        vm.$scope = $scope;
+        $baseController.merge(vm, $baseController);
+        vm.contentService = $cmsContentService;
+        vm.notify = vm.contentService.getNotifier($scope);
+        vm.serverModel = $serverModel;
+        var pageUrl = vm.serverModel.item;
+        vm.header = null;
+        vm.trustSrc = _trustSrc;
+        vm.trustHtml = _trustHtml;
+        vm.headerTemplateUrl = _templateUrl;
+
+
+        _render();
+
+        function _render() {
+
+            vm.contentService.selectAllContentKeyValue(pageUrl, _onGetContentSuccess, _getOnError);
+
+        }
+
+        // ON GET HEADER CONTENT
+        function _onGetContentSuccess(data) {
+            if (data) {
+                vm.notify(function () {
+                    vm.header = data.item;
+                });
+            }
+        }
+
+        // ERROR FUNCTION
+        function _getOnError(jqXHR) {
+            vm.$alertService.error(jqXHR.responseText, "Get Header failed");
+        }
+
+        // SELECT TEMPLATE (LEGACY NG)
+        function _templateUrl() {
+            return '/Scripts/app/cms/cmsContent/templates/_cmsContentHeaderTest.html';
+        }
+
+        // SANITIZE FILES
+        function _trustSrc(src) {
+            return $sce.trustAsResourceUrl(src);
+        }
+
+        // SANITIZE HTML
+        function _trustHtml(src) {
+            return $sce.trustAsHtml(src);
+        }
+
+    }
+})();
